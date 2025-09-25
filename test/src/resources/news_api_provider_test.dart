d520106ba86571dd10338e445ee911488402f33a
arufiand <alfiandannyarmanta@gmail.com>
2025-09-25 12:24:06 +0700

    feat(newsApiProviderTest): write test with mock to fetch item

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -15,4 +15,30 @@ void main() {
 
     expect(ids, [1, 2, 3, 4]);
   });
+  test('fetchItem return an ItemModel instance', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {
+        'id': 123,
+        'deleted': false,
+        'type': 'story',
+        'by': 'testuser',
+        'time': 1678886400,
+        'text': 'This is a test story.',
+        'dead': false,
+        'parent': 1,
+        'kids': [],
+        'url': 'http://test.com',
+        'score': 100,
+        'title': 'Test Title',
+        'descendants': 5,
+      };
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(123);
+
+    expect(item.id, 123);
+    expect(item.deleted, false);
+  });
 }
