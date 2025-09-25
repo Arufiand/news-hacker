66943bee9af89bfd69268c97ef4cc8b06c765998
arufiand <alfiandannyarmanta@gmail.com>
2025-09-25 11:51:58 +0700

    feat(newsApiProvider): update adding news Api Provider

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,7 +1,24 @@
 import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+import 'package:news_learning/src/models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
-  fetchTopId() {}
+  Client client = Client();
 
-  fetchItem() {}
+  fetchTopId() async {
+    final response = await client.get('$_root/topstories.json' as Uri);
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json' as Uri);
+    final parser = json.decode(response.body);
+
+    return ItemModel.fromJson(parser);
+  }
 }
