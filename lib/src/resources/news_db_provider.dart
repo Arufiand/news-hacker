6f3ea9b2a71faea95fab90498cc46cafe75b86b6
arufiand <alfiandannyarmanta@gmail.com>
2025-09-25 13:28:18 +0700

    feat(newsDbProvider): create init function

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -5,20 +5,37 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 
-// class NewsDbProvider {
-//   Database db;
-//
-//   // init method
-//   init() async {
-//     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-//     final path = join(documentsDirectory.path, 'items.db');
-//
-//     db = await openDatabase(
-//       path,
-//       version: 1,
-//       onCreate: (Database newDb, int version) {
-//
-//       },
-//     );
-//   }
-// }
+class NewsDbProvider {
+  Database db;
+
+  // init method
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'items.db');
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE items 
+            (
+              id INTEGER PRIMARY KEY,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kids BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
+        """);
+      },
+    );
+  }
+}
